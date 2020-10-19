dd1d44c1e8dfb5efcfe1d56a1bd41faa9ae06920
cleminator <alvinclemens@gmail.com>
2020-10-19 13:17:14 -0600

    server runs adding in public folder and burger.js, now going to handle bars

---
+++ public/assets/Burger.js
@@ -0,0 +1,12 @@
+// this is used to display the correct information in handle bars
+$(function(){
+    $(".add-burger").on("click", function(event){
+        var id = $(this).date("id");
+        var newBurger = $(this).data("newBerger");
+
+        var newBergerState = {
+            // enter DB information when you ahve it 
+        }
+})
+
+})
