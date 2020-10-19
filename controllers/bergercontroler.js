b4118d6ea6a567e8deb380e20b732ec80cb3e21e
cleminator <alvinclemens@gmail.com>
2020-10-19 14:33:20 -0600

    modules file added

---
+++ controllers/bergercontroler.js
@@ -0,0 +1,19 @@
+// this page renders the handlebars page
+
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+
+router.get("/", function(req, res) {
+    cat.all(function(data) {
+      var burgerObject = {
+        burger: data
+      };
+      console.log(burgerObject);
+      res.render("index", burgerObject);
+    });
+  });
